3cadd0d12fb28e367926560abba8bfc40ed2a7eb
Jonathan Lin <jlin1999@gmail.com>
2018-11-16 00:14:24 -0500

    Update calipot_Arduino/calipot_Arduino.ino
    Added prank

--- calipot_Arduino/calipot_Arduino.ino
+++ calipot_Arduino/calipot_Arduino.ino
@@ -2,11 +2,74 @@
 
 const int potentiometer = A6;
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int buzzer = 9;
+const int led = 6;
 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+void prank()
+{
+  lcd.setCursor(0, 0);
+  lcd.print("ARMED");
+  delay(1500);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("DETONATION IN:");
+  lcd.setCursor(0, 1);
+  
+  int sinceToneStart = 0;//Number of ms since tone started
+  int numCycles = 0;//Number of 1ms cycles
+  int numTones = 0;//Number of tone and LED flashes
+  int nextDelay = 1000;//Interval to next tone and LED flash
+  int increment = 0;//Current number of 1ms cycles to next tone and LED flash
+  
+  for (int i=0; i<8000; i++)
+  {
+    if (increment == nextDelay)
+    {
+      digitalWrite(led, HIGH);
+      tone(buzzer, 640);
+      sinceToneStart = 0;
+      nextDelay = 1000*exp(-0.16*numTones);
+
+      numTones++;
+      increment = 0;
+    }
+
+    if (sinceToneStart == 50 || i == 7999)
+    {
+      noTone(buzzer);
+      digitalWrite(led, LOW);
+    }
+
+    if (numCycles%1000 == 0)
+    {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("DETONATION IN:");
+      lcd.setCursor(0, 1);
+      lcd.print((8000-i)/1000.0);
+      lcd.print(" sec");
+    }
+   
+    increment++;
+    numCycles++;
+    sinceToneStart++;
+    delay(1);
+  }
+
+  lcd.clear();
+  lcd.print("BOOM!");
+  delay(3000);
+}
+
 void setup() {
   pinMode(potentiometer, INPUT);
+
+  pinMode(buzzer, OUTPUT);
+  pinMode(led, OUTPUT);
+
+  Serial.begin(9600);
 
   lcd.begin(16, 2);
 
@@ -20,8 +83,9 @@ void setup() {
     lcd.print("#");
     delay(65);
   }
+  lcd.clear();
   
-  Serial.begin(9600);
+  prank();
 }
 
 void loop() {
